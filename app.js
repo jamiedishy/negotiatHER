65bb388ced1d5c4c2d7918405308be446b3d5aa3
root <root@MSI.localdomain>
2020-01-17 13:52:39 -0500

    mailchimp api

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const request = require("request");
-const port = 2000
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const passport = require("passport");
+const port = 2000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
@@ -44,23 +44,25 @@ app.post("/", function(req, res) {
 
     request(option, function(error, response, body) {
         if (error) {
-           // res.send("There was an error signing up. Please try again");
-           res.sendFile(__dirname + "/index.html")
+           res.send("There was an error signing up. Please try again");
         }
         else {
             if (response.statusCode == 200) {
-                // res.send("Successfully subscribed!");
-               
+                // successfully hits mailchimp server
                 console.log(response.statusCode);
+                
+                // unless add the below info, the user is not added to mailchimp list for signup.
+                // currently, resort to 'err'... so temporarily sendFILE to portfolio.html given err
+                // needs to be fixed
                 req.login(data.members.user, function(err) {
                     if (err) {
                         //res.status(400).send(err);
                         res.sendFile(__dirname + "/portfolio.html")
-                        console.log("theres an error")
+                        console.log("theres an error");
                     }
                     else {
                         res.sendFile(__dirname + "/portfolio.html")
-                        console.log(res.json(data.members.user))
+                        console.log(res.json(data.members.user));
                     }
                 });
             }
@@ -70,4 +72,4 @@ app.post("/", function(req, res) {
 })
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
