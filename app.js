d24b4e73797505215ab7ee5a800bb3e9afca8d41
root <root@MSI.localdomain>
2020-01-19 23:19:20 -0500

    using ejs

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const request = require("request");
 const bodyParser = require("body-parser");
-const passport = require("passport");
+const ejs = require("ejs");
 const port = 2000;
-
-app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.set('view engine', 'ejs');
 
+app.get('/', function(req, res) {
+res.render("home");
+});
 
 app.post("/", function(req, res) {
 
@@ -22,7 +22,7 @@ app.post("/", function(req, res) {
     var data = {
         members: [
             {email_address: email,
-            user: 'user: 0378c0d592e27ab50c41300827733253-us4',
+            user: 'user: be427d53261e47fd322e51c08af35f22-us4',
             status: "subscribed",
             merge_fields : {
                 FNAME: firstName,
@@ -37,7 +37,7 @@ app.post("/", function(req, res) {
         url: "https://us4.api.mailchimp.com/3.0/lists/bc3321e782",
         method: "POST",
         headers: {
-            "Authorization": "jamie1 0378c0d592e27ab50c41300827733253-us4"
+            "Authorization": "jamie1 be427d53261e47fd322e51c08af35f22-us4"
         },
         body: jsonData
     };
@@ -49,12 +49,22 @@ app.post("/", function(req, res) {
         else {
             if (response.statusCode == 200) {
                 // successfully hits mailchimp server
-                res.sendFile(__dirname + "/portfolio.html")
+                res.redirect("/portfolio");
             }
         }
     })
     
 })
 
+app.get("/portfolio", function(req, res) {
+    res.render("portfolio");
+})
+
+app.get("/questionaire", function(req, res) {
+    res.render("questionaire");
+})
+app.post("/questionaire", function(req, res) {
+    res.redirect("/");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
